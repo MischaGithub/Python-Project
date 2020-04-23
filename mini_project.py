ee2ac348ba927961f4b356e97c0c5ba98eabdaf8
MischaGithub <mischa.achmat120212@gmail.com>
2020-04-23 05:27:07 -0700

    third commit

---
+++ mini_project.py
@@ -0,0 +1,19 @@
+
+def get_users_age(age):
+
+     """
+     >>> age = int(18)
+     >>> get_users_age(age)
+     True
+     """
+
+     if age >= 18:
+         return True
+     else:
+         return False
+
+get_users_age(18)
+
+
+
+
